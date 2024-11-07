15eeb11d4a7d6e370275cc21d97b99feb37cc4ef
Commonerd <92278529+Commonerd@users.noreply.github.com>
2024-11-07 11:05:14 +0000

    restore : 764c34af8856cdc53b314582f54f01a1d0fe9eb7

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,26 @@ import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Auth } from './components/Auth'
 import { Todo } from './components/Todo'
-import './i18n'
-
 import axios from 'axios'
 import { CsrfToken } from './types'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { useTranslation } from 'react-i18next'
-import { useState } from 'react'
-import NetworkForm from './components/NetworkForm'
-import Register from './components/Register'
-import Login from './components/Login'
-import Map from './components/Map'
 
 function App() {
   useEffect(() => {
     axios.defaults.withCredentials = true
     const getCsrfToken = async () => {
       const { data } = await axios.get<CsrfToken>(
-        `${process.env.REACT_APP_API_URL}/csrf`,
+        `${process.env.REACT_APP_API_URL}/csrf`
       )
       axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
     }
     getCsrfToken()
   }, [])
-
-  const { t } = useTranslation()
-  const [user, setUser] = useState<{ email: string; isLoggedIn: boolean }>({
-    email: '',
-    isLoggedIn: false,
-  })
   return (
     <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <Header user={user} setUser={setUser} />{' '}
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/todo" element={<Todo />} />
-            {/* 잠정 */}
-            <Route path="/" element={<Map user={user} setUser={setUser} />} />
-            <Route path="/add-network" element={<NetworkForm />} />
-            <Route path="/login" element={<Login setUser={setUser} />} />
-            <Route path="/register" element={<Register setUser={setUser} />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </BrowserRouter>
   )
 }
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,18 +1,12 @@
 import { useState, FormEvent } from 'react'
 import { CheckBadgeIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
 import { useMutateAuth } from '../hooks/useMutateAuth'
-import { useTranslation } from "react-i18next";
 
 export const Auth = () => {
-  const { t, i18n } = useTranslation();
   const [email, setEmail] = useState('')
   const [pw, setPw] = useState('')
   const [isLogin, setIsLogin] = useState(true)
   const { loginMutation, registerMutation } = useMutateAuth()
-
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-  };
 
   const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -40,10 +34,9 @@ export const Auth = () => {
       <div className="flex items-center">
         <CheckBadgeIcon className="h-8 w-8 mr-2 text-blue-500" />
         <span className="text-center text-3xl font-extrabold">
-          MigNetVu 
+          Todo app by React/Go(Echo)
         </span>
-         <span className="text-sm font-bold">　{t("appSubName")} </span>
-        </div>
+      </div>
       <h2 className="my-6">{isLogin ? 'Login' : 'Create a new account'}</h2>
       <form onSubmit={submitAuthHandler}>
         <div>
