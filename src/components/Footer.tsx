093b00e0e1202e966de852a42ae0d74680dac2d7
[Commonerd] <[syh712@gmail.com]>
2025-03-17 15:52:23 +0900

    update : footer with operation time and inquiry

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,12 @@ const Footer: React.FC = () => {
     <footer className="bg-gray-800 text-white p-4">
       <div className="container mx-auto text-center">
         <p>
-          &copy; 2024 {t("Dr. Song Younghwa")}. {t("allRightsReserved")}
+          &copy; 2024 {t("Dr. Song Younghwa")}. {t("allRightsReserved")} /{" "}
+          09:00-18:00 /
+          <a href="mailto:41m@naver.com" className="text-blue-400">
+            {" "}
+            41m@naver.com
+          </a>
         </p>
       </div>
     </footer>
