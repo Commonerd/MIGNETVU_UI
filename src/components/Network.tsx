2e0319488d5b67da6dcf1ea6cd805e25f63805de
[Commonerd] <[syh712@gmail.com]>
2025-04-04 08:21:03 +0900

    Update: Adjest size of delete icon

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -743,7 +743,7 @@ export const Network = () => {
                       viewBox="0 0 24 24"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="w-4 h-4"
+                      className="w-5 h-5"
                     >
                       <path
                         strokeLinecap="round"
