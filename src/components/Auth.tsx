ec121776c83b556f5a758c44236fac181067060c
[Commonerd] <[syh712@gmail.com]>
2025-05-24 22:14:12 +0900

    remove: console.log

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -42,13 +42,13 @@ export const Auth = () => {
   }, [])
 
   const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
-    // e.preventDefault()
-    // if (!csrfLoaded) {
-    //   alert(
-    //     "The CSRF token hasn't loaded yet. Please wait for just 30 seconds!",
-    //   )
-    //   return
-    // }
+    e.preventDefault()
+    if (!csrfLoaded) {
+      alert(
+        "The CSRF token hasn't loaded yet. Please wait for just 30 seconds!",
+      )
+      return
+    }
     if (isLogin) {
       loginMutation.mutate(
         { id: 0, email, password: pw, role: "", name: "" },
@@ -87,12 +87,12 @@ export const Auth = () => {
 
   // 구글 로그인 성공 시 처리 함수
   const handleGoogleLogin = async (token: string) => {
-    // if (!csrfLoaded) {
-    //   alert(
-    //     "The CSRF token hasn't loaded yet. Please wait for just 30 seconds!",
-    //   )
-    //   return
-    // }
+    if (!csrfLoaded) {
+      alert(
+        "The CSRF token hasn't loaded yet. Please wait for just 30 seconds!",
+      )
+      return
+    }
     try {
       const res = await axios.post(
         `${process.env.REACT_APP_API_URL}/auth/google`,
@@ -100,7 +100,6 @@ export const Auth = () => {
         { withCredentials: true },
       )
       // 로그인 성공 시 전역 상태에 사용자 정보 저장
-      console.log(res.data)
       setUser({
         id: res.data.user.id,
         email: res.data.user.email,
